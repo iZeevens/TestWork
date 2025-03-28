a19644b30f27fdc8417112dc06f2081ab23d377c
iZeevens <129598659+iZeevens@users.noreply.github.com>
2025-03-28 23:35:22 +0500

    feat: add few decor

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,6 +1,9 @@
 import styles from "./Game.module.css";
 import Header from "@components/Header/Header";
 import HouseGroup from "@components/HouseGroup/HouseGroup";
+import Avatar from "@assets/icons/avatar-icon.svg?react";
+import Wood from "@assets/icons/wood.svg?react";
+import Bush1 from "@assets/icons/bush-1.svg?react";
 
 function GamePage() {
   return (
@@ -8,6 +11,13 @@ function GamePage() {
       <Header />
       <main className={styles.content}>
         <HouseGroup />
+        <Avatar className={styles.avatar} />
+        <Wood className={`${styles.wood1} ${styles.decor}`} />
+        <Wood className={`${styles.wood2} ${styles.decor}`} />
+        <Wood className={`${styles.wood3} ${styles.decor}`} />
+        <Wood className={`${styles.wood4} ${styles.decor}`} />
+        <Wood className={`${styles.wood5} ${styles.decor}`} />
+        <Bush1 className={`${styles.bush1} ${styles.decor}`} />
       </main>
     </div>
   );
